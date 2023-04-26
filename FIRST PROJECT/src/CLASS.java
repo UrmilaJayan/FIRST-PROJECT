f44e2fc1bfab1b99334cb0a7a7d1e3ce2c83e1df
jayan <jayan@URMILA>
2023-04-26 16:08:10 +0530

    hai

---
+++ FIRST PROJECT/src/CLASS.java
@@ -0,0 +1,7 @@
+
+public class CLASS {
+	public static void main(String[] args) {
+		System.out.println("This is my first java program");
+	}
+
+}
---
+++ FIRST PROJECT/src/operators.java
@@ -0,0 +1,9 @@
+
+public class operators {
+	public static void main(String[] args) {
+		int a=5;
+		for(int i=0;i<=10;i++)
+		{System.out.println( "5 *"+i +"="+(5*i));
+	}
+	}
+}
